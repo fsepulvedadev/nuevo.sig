a3604720f380988fdeffeffa98617d74c6d90fd2
Francisco Sepulveda <fsepulveda@copade.neuquen.gov.ar>
2023-04-12 11:03:31 -0300

    agregado loader, funcion imprimir y descargar capas

--- main.js
+++ main.js
@@ -7,11 +7,43 @@ const panelCapasActivasElement = document.getElementById("panelCapasActivas");
 const panelCapasActivasBtn = document.getElementById("panelCapasActivasBtn");
 const listaCapasActivas = document.getElementById("listaCapasActivas");
 const inputOpacidad = document.getElementById("opacidad");
+const spinner = document.getElementById("spinner");
 const parser = new XMLParser();
 let capas = [];
 let capaSeleccionada = "";
+let capasTotalmenteCargadas = 0;
 let opacidad = 0;
 let capasDePruebaActivas = [];
+const mapasBase = {
+  argenmapBase: L.tileLayer(
+    "https://wms.ign.gob.ar/geoserver/gwc/service/tms/1.0.0/capabaseargenmap@EPSG%3A3857@png/{z}/{x}/{-y}.png",
+    {
+      attribution:
+        "<a href='https://www.ign.gob.ar/AreaServicios/Argenmap/Introduccion'>Instituto Geográfico Nacional</a>",
+    }
+  ),
+  argenmapGris: L.tileLayer(
+    "https://wms.ign.gob.ar/geoserver/gwc/service/tms/1.0.0/mapabase_gris@EPSG%3A3857@png/{z}/{x}/{-y}.png",
+    {
+      attribution:
+        "<a href='https://www.ign.gob.ar/AreaServicios/Argenmap/Introduccion'>Instituto Geográfico Nacional</a>",
+    }
+  ),
+  argenmapTopografico: L.tileLayer(
+    "https://wms.ign.gob.ar/geoserver/gwc/service/tms/1.0.0/mapabase_topo@EPSG%3A3857@png/{z}/{x}/{-y}.png",
+    {
+      attribution:
+        "<a href='https://www.ign.gob.ar/AreaServicios/Argenmap/Introduccion'>Instituto Geográfico Nacional</a>",
+    }
+  ),
+  argenmapOscuro: L.tileLayer(
+    "https://wms.ign.gob.ar/geoserver/gwc/service/tms/1.0.0/argenmap_oscuro@EPSG%3A3857@png/{z}/{x}/{-y}.png",
+    {
+      attribution:
+        "<a href='https://www.ign.gob.ar/AreaServicios/Argenmap/Introduccion'>Instituto Geográfico Nacional</a>",
+    }
+  ),
+};
 
 inputOpacidad.addEventListener("change", () => {
   opacidad = inputOpacidad.value * 0.01;
@@ -54,12 +86,22 @@ const traerCapas = async () => {
           transparent: true,
         }
       );
+      setLoadEvent(nuevaCapa);
 
       capasDePruebaActivas.push(nuevaCapa);
     }
   });
   console.log(capasDePruebaActivas);
   capasDePruebaActivas[3].setOpacity(0.2);
+};
+
+const setLoadEvent = (layer) => {
+  layer.on("load", () => {
+    if (spinner.classList.contains("flex")) {
+      spinner.classList.remove("flex");
+      spinner.classList.add("hidden");
+    }
+  });
 };
 
 const cargarCapasActivas = () => {
@@ -91,6 +133,8 @@ const selectorDeOpacidad = () => {
     }
   });
 };
+
+const selectorDeCapasBase = (capa) => {};
 
 const moverseAlaNuevaCapa = (capa) => {
   capas.map((e) => {
@@ -151,6 +195,8 @@ const toggleCapa = async (capa, element) => {
         element.classList = "badge badge-ghost cursor-pointer select-none";
         cargarCapasActivas();
       } else {
+        spinner.classList.remove("hidden");
+        spinner.classList.add("flex");
         map.addLayer(e);
         moverseAlaNuevaCapa(capa);
         capasActivas.push(e.options.layers[0]);
@@ -193,3 +239,93 @@ L.tileLayer("https://tile.openstreetmap.org/{z}/{x}/{y}.png", {
 }).addTo(map);
 
 traerCapas();
+
+L.easyPrint({
+  title: "Imprimir mapa",
+  position: "topright",
+  sizeModes: ["Current", "A4Landscape", "A4Portrait"],
+  filename: "mapa",
+}).addTo(map);
+
+const cambiarNombreDeBotonesPrint = () => {
+  const botones = document.querySelectorAll(".easyPrintSizeMode");
+  Array.from(botones).map((e) => {
+    if (e.title === "Current Size") {
+      e.title = "Tamaño actual";
+    }
+    if (e.title === "A4 Landscape") {
+      e.title = "A4 Horizontal";
+    }
+    if (e.title === "A4 Portrait") {
+      e.title = "A4 Vertical";
+    }
+  });
+};
+
+cambiarNombreDeBotonesPrint();
+
+const selectorDescargaCapa = document.getElementById("descargarCapa");
+selectorDescargaCapa.addEventListener("change", (e) => {
+  downloadFile(e.target);
+});
+
+const downloadFile = (format) => {
+  if (format.value === "descargar") {
+    return;
+  }
+
+  switch (format.value) {
+    case "shapefile":
+      window.location =
+        "http://giscopade.neuquen.gov.ar/geoserver/Copade/ows?service=WFS&version=1.0.0&request=GetFeature&typeName=" +
+        capaSeleccionada.Name +
+        "&outputformat=SHAPE-ZIP&SRSNAME=EPSG:4326";
+      break;
+
+    case "json":
+      window.open(
+        "http://giscopade.neuquen.gov.ar/geoserver/Copade/ows?service=WFS&version=1.0.0&request=GetFeature&typeName=" +
+          capaSeleccionada.Name +
+          "&outputformat=JSON&SRSNAME=EPSG:4326"
+      );
+
+      break;
+
+    case "csv":
+      window.location =
+        "http://giscopade.neuquen.gov.ar/geoserver/Copade/ows?service=WFS&version=1.0.0&request=GetFeature&typeName=" +
+        capaSeleccionada.Name +
+        "&outputFormat=csv";
+      break;
+    case "jsonp":
+      window.open(
+        "http://giscopade.neuquen.gov.ar/geoserver/Copade/ows?service=WFS&version=1.0.0&request=GetFeature&typeName=" +
+          capaSeleccionada.Name +
+          "&outputFormat=application/json"
+      );
+      break;
+    case "kml":
+      window.location =
+        "http://giscopade.neuquen.gov.ar/geoserver/Copade/wms/kml?layers=" +
+        capaSeleccionada.Name;
+      break;
+
+    case "gml2":
+      window.open(
+        "http://giscopade.neuquen.gov.ar/geoserver/Copade/ows?service=WFS&version=1.0.0&request=GetFeature&typeName=" +
+          capaSeleccionada.Name +
+          "&info_format=application/vnd.ogc.gml"
+      );
+      break;
+    case "gml3":
+      window.open(
+        "http://giscopade.neuquen.gov.ar/geoserver/Copade/ows?service=WFS&version=1.0.0&request=GetFeature&typeName=" +
+          capaSeleccionada.Name +
+          "&outputFormat=application/vnd.ogc.gml/3.1.1"
+      );
+      break;
+
+    default:
+      break;
+  }
+};
